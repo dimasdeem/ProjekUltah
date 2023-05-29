9a6283d53835069ab085febe6ee0c648a9209ea4
HαckͥDeͣeͫm <92859246+dimasdeem@users.noreply.github.com>
2023-05-29 15:21:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let countDown = new Date('Jun 4, 2023 00:00:00').getTime(),
     let now = new Date().getTime(),
       distance = countDown - now;
 
-    // document.getElementById('days').innerText = Math.floor(distance / (day)),
+    document.getElementById('days').innerText = Math.floor(distance / (day)),
     document.getElementById('hours').innerText = Math.floor(distance / (hour)),
       document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
       document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
